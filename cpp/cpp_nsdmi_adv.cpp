f527e4718d720b46416a4c817ead58bf2c434c68
Bartłomiej Filipek <joebaf@gmail.com>
2017-10-04 21:13:32 +0200

    fix adv

--- cpp/cpp_nsdmi_adv.cpp
+++ cpp/cpp_nsdmi_adv.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 
+class SimpleType
+{
+private:
+	int a { 1 };    // << wow!
+	int b { 1 };    // << wow2!
+	std::string name { "string" }; // wow3!
+
+public:
+	SimpleType() {
+		std::cout << "SimpleType::ctor, {" << a << ", " << b << ", \"" << name << "\"}" << std::endl;
+	}
+	SimpleType(int aa, int bb) : a(aa), b(bb) {
+		std::cout << "SimpleType::ctor(aa, bb), {" << a << ", " << b << ", \"" << name << "\"}" << std::endl;
+	}
+	~SimpleType() { std::cout << "SimpleType::destructor" << std::endl; }
+};
+
 class AdvancedType
 {
 private:
@@ -17,6 +34,7 @@ public:
 
 int main()
 {
+	SimpleType simpleObj;
 	AdvancedType advObj;
 	AdvancedType advObj2(10);
 }
